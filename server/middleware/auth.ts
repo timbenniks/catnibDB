45393f40f4d9565e13c70d874fb2fc4b6919c5ac
Tim Benniks <tbenniks@gmail.com>
2024-01-05 21:23:33 +0100

    Added back api key auth

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,18 +1,18 @@
 export default defineEventHandler((event) => {
-  // const config = useRuntimeConfig(event)
-  // const { 'x-api-key': apiKey } = getRequestHeaders(event)
+  const config = useRuntimeConfig(event)
+  const { 'x-api-key': apiKey } = getRequestHeaders(event)
 
-  // if (!apiKey) {
-  //   throw createError({
-  //     statusCode: 403,
-  //     statusMessage: 'No API key present',
-  //   })
-  // }
+  if (!apiKey) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'No API key present',
+    })
+  }
 
-  // if (apiKey !== config.apiKey) {
-  //   throw createError({
-  //     statusCode: 403,
-  //     statusMessage: 'Wrong API key',
-  //   })
-  // }
+  if (apiKey !== config.apiKey) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Wrong API key',
+    })
+  }
 })
