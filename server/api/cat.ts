b13047ee512d5ce588794e130c89f1110f58311e
Tim Benniks <tbenniks@gmail.com>
2024-01-05 17:48:03 +0100

    cat enrichment on the server

--- server/api/cat.ts
+++ server/api/cat.ts
@@ -1,5 +1,43 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
 import { serverSupabaseClient } from '#supabase/server'
-import { addCatData } from "../../lib/helpers"
+
+async function treatmentsByCat(catId: number, client: SupabaseClient) {
+  const { data } = await client
+    .from("treatments")
+    .select("id, cat_id, date, notes, treatment_type, treatment_outcome")
+    .eq("cat_id", catId);
+
+  return data;
+}
+
+async function weightsByCat(catId: number, client: SupabaseClient) {
+  const { data } = await client
+    .from("weight")
+    .select("id, cat_id, date, weight_gr")
+    .eq("cat_id", catId);
+
+  return data;
+}
+
+async function addFacets(cat: any, treatments: any) {
+  const result: any = [];
+
+  for (const [key, value] of Object.entries(cat)) {
+    if (key === 'protocol_date' && typeof value === 'string') {
+      result.push(key)
+    }
+
+    if (typeof value === 'boolean' && value) {
+      result.push(key)
+    }
+  }
+
+  treatments.forEach((treatment: any) => {
+    result.push(treatment.treatment_type)
+  });
+
+  return result
+}
 
 export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient(event)
@@ -25,8 +63,21 @@ export default defineEventHandler(async (event) => {
     .eq("id", id)
     .single();
 
-  return {
-    apiKey: event.context.apiKey,
-    ...await addCatData(data, client)
+  if (data) {
+    const treatments = await treatmentsByCat(data.id, client)
+    const weights = await weightsByCat(data.id, client)
+    const facets = await addFacets(data, treatments)
+
+    data.treatments = treatments
+    data.weights = weights
+    data.facets = facets
+
+    return data
+  }
+  else {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Cat not found',
+    })
   }
 })
