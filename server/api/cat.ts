cd3491445c5326a2524c0d75d898a4f719f93cf1
Tim Benniks <tbenniks@gmail.com>
2024-01-05 17:57:27 +0100

    check error

--- server/api/cat.ts
+++ server/api/cat.ts
@@ -33,6 +33,7 @@ export default defineEventHandler(async (event) => {
 
   return {
     apiKey: event.context.apiKey,
-    ...await addCatData(data, client)
+    //...await addCatData(data, client)
+    data
   }
 })
